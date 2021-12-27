668053c8cfc038a93f9f817eba89269e45082b34
kimsungwug <kimsungwug123@gmail.com>
2021-12-27 21:38:31 +0900

    solved

---
+++ example/1_greedy/3-2.py
@@ -0,0 +1,9 @@
+n,m,k = map(int,input().split())
+data = list(map(int,input().split()))
+data.sort()
+result=0
+for i in range(1,m+1):
+    if(i%(k+1)==0): result+=data[n-2]
+    else: result+=data[n-1]
+
+print(result)
---
+++ example/1_greedy/3-3.py
@@ -0,0 +1,11 @@
+n,m = map(int,input().split())
+result=0
+min_value=0
+for i in range(n):
+    data=list(map(int,input().split()))
+    min_value=min(data)
+    if(i!=0 and min_value>result):
+        result = min_value
+    elif(i==0): 
+        result = min_value
+print(result)
